1d51241c314192a196e30b08adbb390ac4a43e4c
Gavin Ferise <gmferise@gmail.com>
2021-10-05 14:45:59 -0400

    Create useraccount.UserAccount, create website.base_views.BaseEndpoint

---
+++ useraccount/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from useraccount.models import UserAccount
+
+from django.utils.translation import ugettext_lazy as _
+
+class UserAccountAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        #(_('Linked Data'), {'fields': ('person',)}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'is_staff')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(UserAccount, UserAccountAdmin)
---
+++ useraccount/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.core.mail import send_mail
+from django.core.validators import EmailValidator
+
+from unicodedata import normalize
+
+from useraccount.manager import UserAccountManager
+
+from django.utils.translation import ugettext_lazy as _
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    """
+    | Field    | Details   |
+    | :------- | :-------- |
+    | email    | unique    |
+    | password | 128 chars |
+    | person   | oto Person | # TODO: ADD THIS
+    """
+
+    username_validator = EmailValidator()
+
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        blank=False,
+        error_messages={
+            'unique': _('That email is already in use.'),
+        },
+    )
+
+    # person = models.OneToOneField(
+    #     'familystructure.Person',
+    #     verbose_name=_('person'),
+    #     related_name='person',
+    #     null=True,
+    #     on_delete=models.DO_NOTHING,
+    # )
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserAccountManager()
+
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    class Meta:
+        verbose_name = _('useraccount')
+        verbose_name_plural = _('useraccounts')
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    def __str__(self):
+        return self.email
+    
+    @classmethod
+    def normalize_email(cls, email):
+        return normalize('NFKC', email) if isinstance(email, str) else email
