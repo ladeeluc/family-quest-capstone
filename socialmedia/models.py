00b0650f7e17a26977ca1c8c88b6d367bc5c0f2d
Ladeeluc <laura.hatch.lh@gmail.com>
2021-10-07 15:28:40 -0400

    completed comments updated fields

--- socialmedia/models.py
+++ socialmedia/models.py
@@ -17,10 +17,13 @@ class Post(models.Model):
 class Comment(models.Model):
     body = models.TextField(max_length=140)
     date_time = models.DateTimeField(default=timezone.now)
-    comment_author= models.ForeignKey('useraccount.UserAccount', related_name='comment', on_delete=models.CASCADE, null=True)
+    author= models.OneToOneField('useraccount.UserAccount', related_name='comment', on_delete=models.CASCADE, null=True)
+    post_comment_added = models.ManyToManyField('Post', symmetrical=False,related_name= 'comments',blank=True)
 
     def __str__(self):
         return self.body
+
+# class Reaction(model.Models):
 
 
 
