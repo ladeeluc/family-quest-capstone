2593bbbb5b402a21401411e738ac673ba8042be2
Gavin Ferise <gmferise@gmail.com>
2021-10-08 13:59:05 -0400

    Merge apps, reset migrations

--- directmessaging/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from directmessaging.models import (
-    Chat,
-    Message,
-)
-
-admin.site.register(Chat)
-admin.site.register(Message)
--- webconfig/settings.py
+++ webconfig/settings.py
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
     'website',
     'useraccount',
     'familystructure',
-    'directmessaging',
     'socialmedia',
 ]
 
