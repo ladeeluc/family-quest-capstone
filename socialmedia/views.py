383c93113c0562c867b6853f16071982fbc9c448
Gavin Ferise <gmferise@gmail.com>
2021-10-08 14:35:52 -0400

    Created Chat get and post

--- socialmedia/views.py
+++ socialmedia/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from website.base_views import BaseEndpoint
+from socialmedia.models import (
+    Chat,
+    Message,
+    MessageReaction,
+    MessageNotification,
+)
 
-# Create your views here.
+from json import (
+    loads as hydrate_json,
+)
+
+class ChatEndpoint(BaseEndpoint):
+
+    def get(self, request, chat_id):
+        """Get all messages in this chat"""
+        try:
+            return self.ok({
+                'messages': [
+                {
+                    'content': message.content,
+                    'sent_at': message.sent_at,
+                    'author': message.author,
+                    'reactions': [
+                        {
+                            'reaction_type': reaction.reaction_type,
+                            'reactor': reaction.reactor,
+                        } for reaction in message.reactions.all()
+                    ],
+                } for message in Chat.objects.get(id=chat_id).messages.all()
+            ],
+            })
+        except Chat.DoesNotExist:
+            return self.not_found()
+    
+    def post(self, request, chat_id):
+        """
+        Send a new message to this chat, making the necessary notifications
+
+        Expected JSON:
+        ```js
+        {
+            "content": String,
+        }
+        ```
+        """
+        json = hydrate_json(request.body)
+        try:
+            chat = Chat.objects.get(id=chat_id)
+            message = Message.objects.create(
+                content=json['content'],
+                author=request.user,
+            )
+            for user in chat.members.all():
+                if user != request.user:
+                    MessageNotification.objects.create(
+                        target_user=user,
+                        target_message=message,
+                    )
+            return self.done()
+        except Chat.DoesNotExist:
+            return self.not_found()
+        
--- website/base_views.py
+++ website/base_views.py
@@ -1,14 +1,38 @@
-from django.http import JsonResponse
+from django.http.response import (
+    HttpResponse,
+    HttpResponseBadRequest,
+    HttpResponseForbidden,
+    HttpResponseNotFound,
+    JsonResponse,
+)
 from django.views.generic import View
+
+class HttpResponseResourceCreated(HttpResponse):
+    status_code = 201
 
 class BaseEndpoint(View):
     """
-    Example:
-
-    def get(request):
-        return JsonResponse({
-            'data': list(Model.objects.values())
-        })
+    | Method         | Use           |
+    | :------------- | :------------ | 
+    | self.ok        | Send 200/json |
+    | self.not_ok    | Send 400      |
+    | self.not_found | Send 404      |
+    | self.no_perms  | Send 403      |
     """
+
+    def ok(self, data):
+        return JsonResponse(data)
+    
+    def done(self):
+        return HttpResponseResourceCreated()
+    
+    def not_ok(self):
+        return HttpResponseBadRequest()
+    
+    def not_found(self):
+        return HttpResponseNotFound()
+    
+    def no_perms(self):
+        return HttpResponseForbidden()
     
 
