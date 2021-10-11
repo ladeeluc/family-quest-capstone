058d1d3efe4eae6b7ee2d402ab08747772fe14e7
Gavin Ferise <gmferise@gmail.com>
2021-10-11 16:43:48 -0400

    Revert "change LoginForm to inherit ModelForm"
    This reverts commit 7e1f8a28c475b65d9aacd75586e291bcef22e7ff.

--- useraccount/forms.py
+++ useraccount/forms.py
@@ -1,13 +1,14 @@
 from django import forms
-from useraccount.models import UserAccount
 
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = UserAccount
-        fields = ['email', 'password']
+
+class LoginForm(forms.Form):
+    email = forms.CharField(max_length=50)
+    # this widget/plugin '.PasswordInput' hides the chars with '****'
+    password = forms.CharField(widget=forms.PasswordInput)
 
 class SignupForm(forms.Form):
-    email = forms.CharField(max_length=254, label='Email address')
+    email = forms.CharField(max_length=50)
+    # this widget/plugin '.PasswordInput' hides the chars with '****'
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
     def clean(self):
