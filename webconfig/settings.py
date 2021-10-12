beeb9e2e94518c8694b214a79290ae2f751cede5
KeishaPierre <hopeamyturner@gmail.com>
2021-10-12 12:02:39 -0400

    merge

--- webconfig/settings.py
+++ webconfig/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ["http.localhost8000",]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
