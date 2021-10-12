4156a3bbe6e164e6e2ec4cdeb857ec613683786d
Ladeeluc <laura.hatch.lh@gmail.com>
2021-10-11 21:40:17 -0400

    made profile page for member,unable to save form to db from page

--- memberProfile/models.py
+++ memberProfile/models.py
@@ -1,3 +1,5 @@
 from django.db import models
+from familystructure.models import Person,Relation
+from useraccount.models import UserAccount
 
-# Create your models here.
+
--- webconfig/urls.py
+++ webconfig/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from website import views as frontend
+from memberProfile.views import add_member,member_detail
 
 from socialmedia.views import (
     ChatEndpoint,
@@ -33,5 +34,8 @@ urlpatterns = [
     path('logout/',frontend.Logout.as_view(),name='logout'),
     path('signup/',frontend.Signup.as_view(),name='signup'),
     path('login/',frontend.Login.as_view(),name='login'),
+    path('profile/',add_member , name='profile'),
+    path('profile/<int:id>/', member_detail),
+
 ]
 
