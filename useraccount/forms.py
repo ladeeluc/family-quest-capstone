b979097322b4f0a6eede577083868082695bcf25
Ladeeluc <laura.hatch.lh@gmail.com>
2021-10-14 12:40:20 -0400

    attempted to fix bug, repriportized anothertix

--- useraccount/forms.py
+++ useraccount/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from familystructure.models import Person, Relation
 from useraccount.models import UserAccount
+from django.core.validators import validate_email
 
 class LoginForm(forms.Form):
     email = forms.CharField(max_length=50)
@@ -12,12 +13,16 @@ class SignupForm(forms.Form):
     # this widget/plugin '.PasswordInput' hides the chars with '****'
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
+
+
     
     def clean(self):
         if self.cleaned_data['password'] != self.cleaned_data['confirm_password']:
             self.add_error('password','')
             self.add_error('confirm_password','Passwords do not match. Please try again.')
         return self.cleaned_data
+
+
 
 class AddPersonForm(forms.ModelForm):
     class Meta:
