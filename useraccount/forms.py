c6e6a228c9ef3dd01c9bb66870644cf0cbfe61a5
Gavin Ferise <gmferise@gmail.com>
2021-10-14 13:23:50 -0400

    Email inputs now use EmailField, added extra error condition for registration

--- useraccount/forms.py
+++ useraccount/forms.py
@@ -1,28 +1,22 @@
 from django import forms
-from familystructure.models import Person, Relation
-from useraccount.models import UserAccount
+from familystructure.models import Person
 from django.core.validators import validate_email
 
 class LoginForm(forms.Form):
-    email = forms.CharField(max_length=50)
+    email = forms.EmailField()
     # this widget/plugin '.PasswordInput' hides the chars with '****'
     password = forms.CharField(widget=forms.PasswordInput)
 
 class SignupForm(forms.Form):
-    email = forms.CharField(max_length=50)
+    email = forms.EmailField()
     # this widget/plugin '.PasswordInput' hides the chars with '****'
     password = forms.CharField(widget=forms.PasswordInput)
     confirm_password = forms.CharField(widget=forms.PasswordInput)
-
-
-    
     def clean(self):
         if self.cleaned_data['password'] != self.cleaned_data['confirm_password']:
             self.add_error('password','')
             self.add_error('confirm_password','Passwords do not match. Please try again.')
         return self.cleaned_data
-
-
 
 class AddPersonForm(forms.ModelForm):
     class Meta:
