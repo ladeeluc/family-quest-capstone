e8ee649e32bdb73c516c957999876925ec735ff4
Riley Ferise <raferise@gmail.com>
2021-10-19 12:23:49 -0400

    add post detail; UI cleanup

--- socialmedia/forms.py
+++ socialmedia/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.db.models import fields
+from .models import Comment
 
 class BasePostForm(forms.Form):
     title = forms.CharField(max_length=50)
@@ -8,3 +10,9 @@ class TextPostForm(BasePostForm):
 
 class ImagePostForm(BasePostForm):
     post_photo = forms.FileField()
+
+class AddCommentForm(forms.Form):
+    body = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Comment...'}), label="Leave a comment")
+    class Meta:
+        model = Comment
+        fields = ['body']
